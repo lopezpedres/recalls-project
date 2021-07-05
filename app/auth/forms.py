5dc470c28a2692ba791a3da99b6da00c4092e3b0
Miguel <lopezpedres@gmail.com>
2021-07-04 22:10:26 -0700

    Made Singup form, Starting Users Model and Singup html

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,11 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
 
-class LoginForm():
+class SignForm():
     name = StringField("Name", validators=[DataRequired(), Length(max=64)])
     email = PasswordField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    admin = BooleanField("Admin", validators=[]
+    admin = BooleanField("Admin", validators=[])
+
+class LoginForm():
+    email = PasswordField("Email", validators=[DataRequired(), Email])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("RememberMe")
+    
+
+
 
 
 
--- app/auth/models.py
+++ app/auth/models.py
@@ -1,2 +1,29 @@
 from flask_login import UserMixin
+from app import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def set_password():
+        pass
+
+    def check_password():
+        pass
+
+    def get_by_email():
+        pass
+
+    def save():
+        pass
+
+
+
 
