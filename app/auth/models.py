8449afb35feb0e4690b32cfb7cdb369f3cdddf85
Miguel <lopezpedres@gmail.com>
2021-07-05 21:36:49 -0700

    Finishing User model, added Signup route

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,28 +1,33 @@
 from flask_login import UserMixin
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    admin = db.Column(db.Boolean)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User: {self.email}>'
 
-    def set_password():
-        pass
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
 
-    def check_password():
-        pass
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-    def get_by_email():
-        pass
-
-    def save():
-        pass
+    @staticmethod
+    def get_by_email(email):
+        return User.query.filter(email=email).first()
 
 
 
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,15 @@
 from . import auth_bp
-from.forms import LoginForm
+from .models import User
+from.forms import LoginForm, SignupForm
+import logging
 from werkzeug.urls import url_parse
 from flask_login import login_user, log_out
 from flask import current_user, redirect, request, url_for, render_template
 
 
+logger = logging.getLogger(__name__)
 
-@auth_bp.route('/login', methods= ['GET', 'POST'])
+@auth_bp.route('/login/', methods= ['GET', 'POST'])
 def go_login():
     if current_user.is_authenticated:
         return redirect('index')
@@ -20,9 +23,37 @@ def go_login():
             next_page= request.args.get(url_for('public.index'))
 #Como funciona el next page?! Se que es para darle seguridad a la pagina pe
             if not next_page or url_parse(next_page).netlog !='':
-                return redirect('index.html')
+                return redirect('index')
 
     return render_template('auth/login.html', form=form)
+
+@auth_bp.route('/signup/', methods=['GET','POST'])
+def go_signup():
+    form=SignupForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        email=form.email.data
+        password=form.password.data
+
+        user=User.get_by_email(email)
+        error=None
+        if user is not None:
+            error=f'La cuenta {email} ya existe.'
+        else:
+            user=User(name=name, email=email)
+            user.set_password(password)
+            user.save()
+
+            login_user(user, remember=True)
+            next_page= request.args.get(url_for('index'))
+#Como funciona el next page?! Se que es para darle seguridad a la pagina pe
+            if not next_page or url_parse(next_page).netlog !='':
+                return redirect('index')
+    return render_template('auth/signup', form=form, error=error)
+        
+
+
+
 
 
 
