3fe665b6efb5ce6f00b715d5b4a40c4c3dfb7a12
Miguel <lopezpedres@gmail.com>
2021-07-19 20:20:56 -0700

    Started the order model and the order details model

--- app/sales/models.py
+++ app/sales/models.py
@@ -1,8 +1,10 @@
 #creating database for clients
+from sqlalchemy.orm import defaultload
 from app import db
 import datetime
 
 class Customers(db.Model):
+
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String, nullable=False) 
 	email = db.Column(db.String, nullable=False)
@@ -14,6 +16,12 @@ class Customers(db.Model):
 	def save(self):
 		db.session.add(self)
 		db.session.commit()
+
+	def edit(self):
+		pass
+
+	def delete(self):
+		pass
 	
 	@staticmethod
 	def get_customer_by_name(name):
@@ -21,4 +29,50 @@ class Customers(db.Model):
 	
 	@staticmethod
 	def get_all_customers():
-		return Customers.session.get_all() 
+		return Customers.query.all() 
+
+	
+class Orders(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	customer_id = db.Column(db.String, db.ForeignKey('Customers.id', ondelete='CASCADE'), nullable=False)
+	pay_method = db.Column(db.String, nullable=False)
+	order_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+	shipping_date = db.Column(db.DateTime)
+	order_details_id = db.relationship('Order_details',
+	backref='orders',
+	lazy=True,
+	cascade='all, delete-orphan',
+	order_by='asc(Order_details.created)')
+
+	status = db.Column(db.Boolean, default=False)
+
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def edit(self):
+		pass
+
+	def delete(self):
+		pass
+
+
+	@staticmethod
+	def get_orders():
+		return Orders.query.all()
+	
+
+class Order_details(db.Model):
+	"""Creates a table with the products within an order"""
+	id = db.Column(db.Integer, primary_key = True )
+	product = db.Column(db.String, db.ForeignKey('products.id'), ondelete='CASCADE', nullable=False ) #TODO create products table with their sizes example: 'Chocolate-Hazelnut 100 G'
+	quantity= db.Column(db.Integer, nullable=False)
+	price = db.Column(db.Float)
+	created = db.Column(db.DateTime, default= datetime.datetime.utcnow)
+
+	
+
+	
+
+
+	
