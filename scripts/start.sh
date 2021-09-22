a748d02811262eee92f665c3a90a9180e1e44b6b
Miguel Lopez <lopezpedres@gmail.com>
2021-09-22 14:44:24 -0700

    Getting ready to merge with dev

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,9 @@ set +a
 # if PYTHON_VENV is set, then activate it
 if [ -n "$PYTHON_VENV" ]; then
   source ./venv/scripts/activate
+  #flask db init
+  #flask db migrate -m "local migrations"
+  #flask db upgrade
 fi
 # this command knows the entrypoint is app/entrypoint.py
 flask run
